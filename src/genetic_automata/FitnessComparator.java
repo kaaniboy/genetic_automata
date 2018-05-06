9845404426ca99ceacf9cdbbd52f5faf2d92ffa8
kaaniboy <kaanaksoyaz@gmail.com>
2018-05-05 22:04:43 -0700

    Genetic algorithm implementation seems to be working

--- src/genetic_automata/FitnessComparator.java
+++ src/genetic_automata/FitnessComparator.java
@@ -9,8 +9,13 @@ public class FitnessComparator implements Comparator<DFA> {
 	@Override
 	// DFAs with higher fitness come first.
 	public int compare(DFA first, DFA second) {
-		if (first.getFitness() > second.getFitness()) {
+		double firstFitness = first.getFitness();
+		double secondFitness = second.getFitness();
+
+		if (firstFitness > secondFitness) {
 			return -1;
+		} else if (firstFitness == secondFitness) {
+			return 0;
 		} else {
 			return 1;
 		}
--- src/genetic_automata/GUI.java
+++ src/genetic_automata/GUI.java
@@ -1,12 +1,61 @@
 package genetic_automata;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class GUI {
+	public static final int TRAINING_SIZE = 100;
+	private static List<String> inputs;
+	private static List<Boolean> expected;
+	
 	public static void main(String[] args) {
-		DFA dfa = new DFA();
-		boolean result = dfa.run("010");
+		createTrainingExamples();
 		
-		System.out.println(result);
+		String[] inputsArray = new String[TRAINING_SIZE];
+		boolean[] expectedArray = new boolean[TRAINING_SIZE];
+		
+		for (int i = 0; i < TRAINING_SIZE; i++) {
+			inputsArray[i] = inputs.get(i);
+			expectedArray[i] = expected.get(i);
+		}
+		
+		GeneticAlgorithm ga = new GeneticAlgorithm(inputsArray, expectedArray);
+		
+		DFA best = ga.runEpochs();
+		System.out.println(best.calculateFitness(inputsArray, expectedArray));
+		
+		System.out.println(best.run("01011"));
+	}
+	
+	// Create training examples for the language: binary numbers that are a multiple of 5.
+	public static void createTrainingExamples() {
+		inputs = new ArrayList<>();
+		expected = new ArrayList<>();
+		
+		int num = 0;
+		// Generate examples that should be accepted (multiple of 5).
+		for (int i = 0; i < TRAINING_SIZE / 2; i++) {
+			// Convert the number to its binary string.
+			inputs.add(Integer.toBinaryString(num));
+			
+			expected.add(true);
+			num += 5;
+		}
+		
+		// Generate examples that should be rejected (not multiple of 5).
+		int count = 0;
+		Random rand = new Random();
+		
+		while (count < TRAINING_SIZE / 2) {
+			num = rand.nextInt(1000);
+			
+			if (num % 5 != 0) {
+				inputs.add(Integer.toBinaryString(num));
+				expected.add(false);
+				count++;
+			}
+		}
 	}
 }
